6c36756c5523c28627dfce24c5eedb57dff72fbe
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-11-24 05:10:06 +0700

    Update BoardifyCommand.php

--- src/KnosTx/Boardify/commands/BoardifyCommand.php
+++ src/KnosTx/Boardify/commands/BoardifyCommand.php
@@ -10,8 +10,13 @@ use pocketmine\plugin\PluginOwnedTrait;
 use KnosTx\Boardify\Main;
 
 class BoardifyCommand extends Command implements PluginOwned{
-    use PluginOwnedTrait;
-    public function __construct(Main $plugin){
+	use PluginOwnedTrait;
+	/**
+	 * Boardify Command Construction
+	 * 
+	 * @param Main $plugin
+	 */
+	public function __construct(Main $plugin){
 		parent::__construct("boardify");
 		$this->setDescription("Boardify Commands");
 		$this->setUsage("/boardify <reload>");
@@ -21,19 +26,19 @@ class BoardifyCommand extends Command implements PluginOwned{
 		$this->owningPlugin = $plugin;
 	}
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
-        if (!$sender->hasPermission("boardify.command")) {
-            $sender->sendMessage("§cYou don't have permission to use this command.");
-            return false;
-        }
+	public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+		if (!$sender->hasPermission("boardify.command")) {
+			$sender->sendMessage("§cYou don't have permission to use this command.");
+			return false;
+		}
 
-        if (isset($args[0]) && strtolower($args[0]) === "reload") {
-            $this->plugin->reloadConfig();
-            $sender->sendMessage("§aBoardify configuration reloaded!");
-        } else {
+		if (isset($args[0]) && strtolower($args[0]) === "reload") {
+			$this->plugin->reloadConfig();
+			$sender->sendMessage("§aBoardify configuration reloaded!");
+		} else {
             $sender->sendMessage("§cUsage: /boardify <reload>");
-        }
+		}
 
-        return true;
-    }
+		return true;
+	}
 }
