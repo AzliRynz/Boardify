9be25895081ec32406d50781831c5af4e981501a
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-11-24 14:27:49 +0700

    Update Main.php

--- src/KnosTx/Boardify/Main.php
+++ src/KnosTx/Boardify/Main.php
@@ -7,7 +7,14 @@ use pocketmine\event\Listener;
 use pocketmine\scheduler\Task;
 
 class Main extends PluginBase implements Listener {
+    /**
+     * @var ConfigManager
+     */
     private ConfigManager $configManager;
+
+    /**
+     * @var BoardManager
+     */
     private BoardManager $boardManager;
 
     protected function onEnable(): void {
@@ -17,17 +24,31 @@ class Main extends PluginBase implements Listener {
         $this->boardManager = new BoardManager($this);
 
         $this->getScheduler()->scheduleRepeatingTask(new class($this) extends Task {
-            public function __construct(private Main $plugin) {}
+            /**
+             * @var Main
+             */
+            private Main $plugin;
+
+            public function __construct(Main $plugin) {
+                $this->plugin = $plugin;
+            }
+
             public function onRun(): void {
                 $this->plugin->getBoardManager()->updateBoards();
             }
         }, $this->configManager->getUpdateInterval());
     }
 
+    /**
+     * @return ConfigManager
+     */
     public function getConfigManager(): ConfigManager {
         return $this->configManager;
     }
 
+    /**
+     * @return BoardManager
+     */
     public function getBoardManager(): BoardManager {
         return $this->boardManager;
     }
