41df0afa3360af2daff704a763e2a3b40528855c
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-11-24 17:18:19 +0700

    Update Main.php

--- src/KnosTx/Boardify/Main.php
+++ src/KnosTx/Boardify/Main.php
@@ -1,65 +1,49 @@
 <?php
 
-/*
- *
- *   ____                      _ _  __
- *  |  _ \                    | (_)/ _|
- *  | |_) | ___   __ _ _ __ __| |_| |_ _   _
- *  |  _ < / _ \ / _` | '__/ _` | |  _| | | |
- *  | |_) | (_) | (_| | | | (_| | | | | |_| |
- *  |____/ \___/ \__,_|_|  \__,_|_|_|  \__, |
- *                                      __/ |
- *                                     |___/
- * @license MIT
- * @author KnosTx
- * @link https://github.com/KnosTx/Boardify
- *
- *
- */
-
 declare(strict_types=1);
 
 namespace KnosTx\Boardify;
 
-use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\Task;
 
-class Main extends PluginBase implements Listener
+class Main extends PluginBase
 {
-	private ConfigManager $configManager;
+    private ConfigManager $configManager;
+    private BoardManager $boardManager;
 
-	private BoardManager $boardManager;
+    protected function onEnable() : void
+    {
+        $this->saveResource('config.yml');
 
-	protected function onEnable() : void
-	{
-		$this->saveResource('config.yml');
+        $this->configManager = new ConfigManager($this);
+        $this->boardManager = new BoardManager($this);
 
-		$this->configManager = new ConfigManager($this);
-		$this->boardManager = new BoardManager($this);
+        $updateInterval = $this->configManager->getUpdateInterval();
+        $ticksInterval = (int) ($updateInterval * 20);
 
-		$this->getScheduler()->scheduleRepeatingTask(new class ($this) extends Task {
-			private Main $plugin;
+        $this->getScheduler()->scheduleRepeatingTask(new class ($this) extends Task {
+            private Main $plugin;
 
-			public function __construct(Main $plugin)
-			{
-				$this->plugin = $plugin;
-			}
+            public function __construct(Main $plugin)
+            {
+                $this->plugin = $plugin;
+            }
 
-			public function onRun() : void
-			{
-				$this->plugin->getBoardManager()->updateBoards();
-			}
-		}, $this->configManager->getUpdateInterval());
-	}
+            public function onRun() : void
+            {
+                $this->plugin->getBoardManager()->updateBoards();
+            }
+        }, $ticksInterval);
+    }
 
-	public function getConfigManager() : ConfigManager
-	{
-		return $this->configManager;
-	}
+    public function getConfigManager() : ConfigManager
+    {
+        return $this->configManager;
+    }
 
-	public function getBoardManager() : BoardManager
-	{
-		return $this->boardManager;
-	}
+    public function getBoardManager() : BoardManager
+    {
+        return $this->boardManager;
+    }
 }
