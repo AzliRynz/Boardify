c7815f5c169b5018559ab1ecb21e2051a59cd0de
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-11-24 17:17:53 +0700

    Update ConfigManager.php

--- src/KnosTx/Boardify/ConfigManager.php
+++ src/KnosTx/Boardify/ConfigManager.php
@@ -1,22 +1,5 @@
 <?php
 
-/*
- *
- *   ____                      _ _  __
- *  |  _ \                    | (_)/ _|
- *  | |_) | ___   __ _ _ __ __| |_| |_ _   _
- *  |  _ < / _ \ / _` | '__/ _` | |  _| | | |
- *  | |_) | (_) | (_| | | | (_| | | | | |_| |
- *  |____/ \___/ \__,_|_|  \__,_|_|_|  \__, |
- *                                      __/ |
- *                                     |___/
- * @license MIT
- * @author KnosTx
- * @link https://github.com/KnosTx/Boardify
- *
- *
- */
-
 declare(strict_types=1);
 
 namespace KnosTx\Boardify;
@@ -25,26 +8,17 @@ use pocketmine\utils\Config;
 
 class ConfigManager
 {
-	private Config $config;
+    private Config $config;
+    private Main $plugin;
 
-	private Main $plugin;
+    public function __construct(Main $plugin)
+    {
+        $this->plugin = $plugin;
+        $this->config = new Config($this->plugin->getDataFolder() . 'config.yml', Config::YAML);
+    }
 
-	public function __construct(Main $plugin)
-	{
-		$this->plugin = $plugin;
-		$this->config = new Config($this->plugin->getDataFolder() . 'config.yml', Config::YAML);
-	}
-
-	/**
-	 * @return array<string, mixed>
-	 */
-	public function getDefaultBoard() : array
-	{
-		return $this->config->get('default-board', []);
-	}
-
-	public function getUpdateInterval() : int
-	{
-		return $this->config->get('update-interval', 20);
-	}
+    public function getUpdateInterval() : float
+    {
+        return (float) $this->config->get('update-interval', 0.7);
+    }
 }
