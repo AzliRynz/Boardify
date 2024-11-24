62d18938f729f37ee65bd10da92f5b9608d4c79e
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-11-24 14:32:23 +0700

    Update ConfigManager.php

--- src/KnosTx/Boardify/ConfigManager.php
+++ src/KnosTx/Boardify/ConfigManager.php
@@ -5,16 +5,28 @@ namespace KnosTx\Boardify;
 use pocketmine\utils\Config;
 
 class ConfigManager {
-	  private Config $config;
+    /**
+     * @var Config
+     */
+    private Config $config;
 
-    public function __construct(private Main $plugin) {
+    private Main $plugin;
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
         $this->config = new Config($this->plugin->getDataFolder() . "config.yml", Config::YAML);
     }
 
+    /**
+     * @return array<string, mixed>
+     */
     public function getDefaultBoard(): array {
         return $this->config->get("default-board", []);
     }
 
+    /**
+     * @return int
+     */
     public function getUpdateInterval(): int {
         return $this->config->get("update-interval", 20);
     }
