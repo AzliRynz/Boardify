252367e663b29bc2fa7f7e62029e5bb8a7d312e2
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-12-23 17:56:46 +0700

    Update Main.php

--- src/KnosTx/Boardify/Main.php
+++ src/KnosTx/Boardify/Main.php
@@ -1,22 +1,5 @@
 <?php
 
-/*
- *
- *   ____                      _ _  __
- *  |  _ \                    | (_)/ _|
- *  | |_) | ___   __ _ _ __ __| |_| |_ _   _
- *  |  _ < / _ \ / _` | '__/ _` | |  _| | | |
- *  | |_) | (_) | (_| | | | (_| | | | | |_| |
- *  |____/ \___/ \__,_|_|  \__,_|_|_|  \__, |
- *                                      __/ |
- *                                     |___/
- * @license MIT
- * @author KnosTx
- * @link https://github.com/KnosTx/Boardify
- *
- *
- */
-
 declare(strict_types=1);
 
 namespace KnosTx\Boardify;
@@ -29,37 +12,34 @@ class Main extends PluginBase
 	private ConfigManager $configManager;
 	private BoardManager $boardManager;
 
-	protected function onEnable() : void
+	protected function onEnable(): void
 	{
 		$this->saveResource('config.yml');
 
 		$this->configManager = new ConfigManager($this);
 		$this->boardManager = new BoardManager($this);
 
-		$updateInterval = $this->configManager->getUpdateInterval();
-		$ticksInterval = (int) ($updateInterval * 20);
+		$this->getScheduler()->scheduleRepeatingTask(new class ($this->boardManager) extends Task {
+			private BoardManager $boardManager;
 
-		$this->getScheduler()->scheduleRepeatingTask(new class ($this) extends Task {
-			private Main $plugin;
-
-			public function __construct(Main $plugin)
+			public function __construct(BoardManager $boardManager)
 			{
-				$this->plugin = $plugin;
+				$this->boardManager = $boardManager;
 			}
 
-			public function onRun() : void
+			public function onRun(): void
 			{
-				$this->plugin->getBoardManager()->updateBoards();
+				$this->boardManager->updateBoards();
 			}
-		}, $ticksInterval);
+		}, (int)($this->configManager->getUpdateInterval() * 20));
 	}
 
-	public function getConfigManager() : ConfigManager
+	public function getConfigManager(): ConfigManager
 	{
 		return $this->configManager;
 	}
 
-	public function getBoardManager() : BoardManager
+	public function getBoardManager(): BoardManager
 	{
 		return $this->boardManager;
 	}
